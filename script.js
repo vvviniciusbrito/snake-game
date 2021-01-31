f3c7317d6a19a1efa19a6dbca546e1e5695e5f17
Vinicius Silva <vvviniciusbrito@gmail.com>
2021-01-31 17:42:01 -0300

    adição de movimentos da cobrinha

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criarBG(){
     context.fillStyle = "lightblue";
@@ -18,5 +19,27 @@ function criarCobrinha(){
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
-criarBG();
-criarCobrinha();
+
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead ={
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100); //o Comando "setinterval" define um um intervalo para a execução da função iniciarJogo com 100 milisegundos

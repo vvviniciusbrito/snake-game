4c1fd35a241e887ff9a5f69b4292c88b9a431012
Vinicius Silva <vvviniciusbrito@gmail.com>
2021-01-31 19:33:42 -0300

    definir direcoes para a cobrinha

--- script.js
+++ script.js
@@ -20,7 +20,21 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
@@ -42,4 +56,4 @@ function iniciarJogo(){
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 100); //o Comando "setinterval" define um um intervalo para a execução da função iniciarJogo com 100 milisegundos
+let jogo = setInterval(iniciarJogo, 300); //o Comando "setinterval" define um um intervalo para a execução da função iniciarJogo com 100 milisegundos

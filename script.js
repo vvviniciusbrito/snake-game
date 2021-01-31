55795a1f1ae7ae18fd94059a9b1a5f8b8f9796f1
Vinicius Silva <vvviniciusbrito@gmail.com>
2021-01-31 20:47:45 -0300

    Conclusão do jogo com estilização

--- script.js
+++ script.js
@@ -25,10 +25,11 @@ function criarCobrinha(){
 }
 
 function drawFood(){
-    context.fillStyle = "Red";
+    context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box);
 }
 
+//quando um evento acontece, detecta e chama uma função
 document.addEventListener('keydown', update);
 
 function update(event){
@@ -39,10 +40,18 @@ function update(event){
 }
 
 function iniciarJogo(){
+    
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
@@ -56,12 +65,17 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop(); //pop tira o último elemento da lista
+    }else{
+         food.x = Math.floor(Math.random() * 15 + 1) * box;
+         food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead ={
         x: snakeX,
         y: snakeY
-    }
+    };
 
     snake.unshift(newHead);
 }
